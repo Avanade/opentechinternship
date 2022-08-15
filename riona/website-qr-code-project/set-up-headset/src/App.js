c45231baede3484b7d7b2c5664b22770ba76536a
Riona John (c) <riona.john@avanade.com>
2022-08-15 16:32:42 +0100

    App Updated

--- riona/website-qr-code-project/set-up-headset/src/App.js
+++ riona/website-qr-code-project/set-up-headset/src/App.js
@@ -2,33 +2,47 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+//import Sonnet from '../../components/Sonnet';
 
-import {Button, Alert, Breadcrumb, Card} from 'react-bootstrap';
-
-
+import {Navbar, Container, Nav, NavDropdown, Tabs, Tab, Card} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
-        <Card style={{ color: "#000"}}>
-            <Card.Img />
-            <Card.Body>
-                <Card.Title>Card Example</Card.Title>
-                <Card.Text>An example of react bootstrap cards</Card.Text>
-                <Button variant="primary">Read More</Button>
-            </Card.Body>
-        </Card>
-        <Breadcrumb> 
-            <Breadcrumb.Item>Test</Breadcrumb.Item>
-            <Breadcrumb.Item>Test2</Breadcrumb.Item>
-            <Breadcrumb.Item active>Test3</Breadcrumb.Item>
-        </Breadcrumb>
-        <Alert variant="primary">This is a button!</Alert>
-        <Button>Button</Button>
+      <h1>Set up your HoloLens 2</h1>
+
+        <Navbar bg="light" variant="light" expand="lg" fixed="top">
+      <Container>
+        <Navbar.Brand href="#home">Insert Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="top-navbar-nav" />
+        <Navbar.Collapse id="top-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="about-me">About Me</Nav.Link>
+            <Nav.Link href="step-by-step">Step-by-Step</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    <Card style = {{color: "#000"}}>
+      <Card.Header>Step 1</Card.Header>
+        <Card.Body>
+          <Card.Title>Turn the HoloLens on.</Card.Title>
+            <Card.Text>
+              Step 1 is words, more words.
+            </Card.Text>
+        </Card.Body>
+    </Card>
+
         
       </header>
     </div>
+
   );
 }
 
