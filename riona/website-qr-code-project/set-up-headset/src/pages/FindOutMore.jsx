971f89ae4777f7695c381b589bb3b3e1f0746c1e
Riona John (c) <riona.john@avanade.com>
2022-08-30 11:25:56 +0100

    Mentor card.
    Signed-off-by: Riona John (c) <riona.john@avanade.com>

--- riona/website-qr-code-project/set-up-headset/src/pages/FindOutMore.jsx
+++ riona/website-qr-code-project/set-up-headset/src/pages/FindOutMore.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 
 import HeaderAndNavBar from '../components/header';
 import DesignThinkCard from '../components/design_thinking';
 import WebsiteCard from '../components/website_card';
 import CameraCard from '../components/camera_card';
+import MentorCard from '../components/mentor_card';
 
 export default function FindOutMore() {
   return (
@@ -15,22 +16,21 @@ export default function FindOutMore() {
       <HeaderAndNavBar />
       <h1>Find Out More</h1>
       <Container>
-      <Row>
-          <Col>
-            <DesignThinkCard />
-          </Col>
+        <Row>
+          <DesignThinkCard />
         </Row>
         <br />
         <Row>
-          <Col>
-            <WebsiteCard />
-          </Col>
+          <WebsiteCard />
         </Row>
         <br />
         <Row>
-          <Col>
-            <CameraCard />
-          </Col>
+          <CameraCard />
+        </Row>
+        <h1>Overall Internship Experience</h1>
+        <br />
+        <Row>
+          <MentorCard />
         </Row>
       </Container>
     </div>
